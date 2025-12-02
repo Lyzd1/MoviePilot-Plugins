1fc57939e1d775ba2b56ff112a289df60ca15ea6
The falling leaves know <162241377+Lyzd1@users.noreply.github.com>
2025-12-02 20:24:24 +0800

    Update __init__.py

--- plugins.v2/iyuuautoseedplus/__init__.py
+++ plugins.v2/iyuuautoseedplus/__init__.py
@@ -34,7 +34,7 @@ class IYUUAutoSeedPlus(_PluginBase):
     # 插件图标
     plugin_icon = "IYUU.png"
     # 插件版本
-      plugin_version = "2.16"
+    plugin_version = "2.16"
     # 插件作者
     plugin_author = "Lyzd1,jxxghp,CKun"
     # 作者主页
